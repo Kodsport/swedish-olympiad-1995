a80b3e2cb6a780de2f368a0d962d9de9b7918e17
Matistjati <matistjati@outlook.com>
2025-10-15 20:36:12 +0000

    Change scoring text

--- final/basaddition/submissions/accepted/joshua_double.cpp
+++ final/basaddition/submissions/accepted/joshua_double.cpp
@@ -1,49 +1,17 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <sstream>
-#include <algorithm>
-#include <cmath>
-#include <set>
-#include <unordered_set>
-#include <string>
-#include <iterator>
-#include <queue>
-#include <tuple>
-#include <numeric>
-#include <random>
-#include <time.h>
-
-
+#include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define p2 pair<ll, ll>
-#define p3 tuple<ll, ll, ll>
-#define vi vector<ll>
-#define inf 1e9
+using ll = long long;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+using p2 = pair<ll, ll>;
+const ll inf = 1e18;
 
-#define read(a) cin >> a
-#define write(a) cout << (a) << endl
-
-#define readpush(type,a) type temp; read(temp); a.push_back(temp)
-#define readinsert(type,a) type temp; read(temp); a.insert(temp)
-#define setcontains(set, x) (set.find(x) != set.end())
-#define all(a) begin(a),end(a)
-
-#define rep(i, high) for (ll i = 0; i < high; i++)
-#define repe(i, container) for (auto& i : container)
-#define per(i, high) for (ll i = high; i >= 0; i--)
-
-#define ceildiv(x,y) ((x + y - 1) / y)
-
-
-inline void fast()
-{
-    ios::sync_with_stdio(false);
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-}
+#define rep(i,n) for (ll i = 0; i < (n); i++)
+#define repp(i,a,n) for (ll i = (a); i < (n); i++)
+#define repe(i, arr) for (auto& i : arr)
+#define all(x) begin(x),end(x)
+#define sz(x) ((ll)(x).size())
 
 struct line
 {
@@ -66,9 +34,8 @@ double applyOperator(double left, double right, char op)
     case '*':
         return left * right;
     default:
-        write("fuck you");
+        assert(0);
         return -INFINITY;
-        break;
     }
 }
 
@@ -137,21 +104,14 @@ bool check_base(line expression, double base)
 
 int main()
 {
-    fast();
-
-
-
     std::vector<string> operators = { "+","-","/","*" };
 
     vector<line> expressions;
 
-
     line l;
+    cin >> l.left >> l.right >> l.answer;
 
-    read(l.left);
     l.op = '+';
-    read(l.right);
-    read(l.answer);
 
     expressions.push_back(l);
 
